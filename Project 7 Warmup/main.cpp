60e75015c2ffb186c487fff7e365cd136ff7f089
Aylin Henstridge <aylin.henstridge@gmail.com>
2018-05-31 16:38:26 -0700

    Initial Commit

---
+++ Project 7 Warmup/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project 7 Warmup
+//
+//  Created by Aylin Henstridge on 5/31/18.
+//  Copyright © 2018 Aylin Henstridge. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
